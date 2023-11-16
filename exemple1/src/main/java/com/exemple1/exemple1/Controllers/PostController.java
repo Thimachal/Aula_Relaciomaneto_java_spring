54fc1cac2061dcdb95ea53df509a2f515ec48f40
Thimachal <thimachal@gmail.com>
2023-11-16 20:11:01 -0300

    controlller

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
@@ -2,6 +2,8 @@ package com.exemple1.exemple1.Controllers;
 
 import com.exemple1.exemple1.Services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,12 @@ public class PostController {
     private PostService actionPostService;
 
     @GetMapping("/")
-    public void classallPosts(){
-        actionPostService.postAll();
+    public ResponseEntity<?> classallPosts() {
+        try {
+            var request = actionPostService.postAll();
+            return new ResponseEntity<>(request, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
