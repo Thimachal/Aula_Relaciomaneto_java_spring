6cb13983921521ef1c252b929abb12bf291cbb59
Thimachal <thimachal@gmail.com>
2023-11-16 19:15:06 -0300

    repository

--- exemple1/src/main/java/com/exemple1/exemple1/Repositories/PostRepository.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Repositories/PostRepository.java
@@ -2,6 +2,8 @@ package com.exemple1.exemple1.Repositories;
 
 import com.exemple1.exemple1.Models.Post;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends CrudRepository<Post,Long> {
 }
