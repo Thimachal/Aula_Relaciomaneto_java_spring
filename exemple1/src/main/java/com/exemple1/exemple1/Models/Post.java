25eadc6869305bb107583cb5d2f17cb90d05c2cc
Thimachal <thimachal@gmail.com>
2023-11-16 18:43:44 -0300

    entidades

--- exemple1/src/main/java/com/exemple1/exemple1/Models/Post.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/Post.java
@@ -1,9 +1,46 @@
 package com.exemple1.exemple1.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "posts")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    private String namePost;
+    private Double salaryPost;
+
+    public  Post(){
+
+    }
+    public Post(Long postId, String namePost, Double salaryPost) {
+        this.postId = postId;
+        this.namePost = namePost;
+        this.salaryPost = salaryPost;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public String getNamePost() {
+        return namePost;
+    }
+
+    public void setNamePost(String namePost) {
+        this.namePost = namePost;
+    }
+
+    public Double getSalaryPost() {
+        return salaryPost;
+    }
+
+    public void setSalaryPost(Double salaryPost) {
+        this.salaryPost = salaryPost;
+    }
 }
