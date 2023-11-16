69daa66159640b2636c9143b86fdadc91ba80161
Thimachal <thimachal@gmail.com>
2023-11-16 18:48:54 -0300

    entidades

--- exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
@@ -1,4 +1,10 @@
 package com.exemple1.exemple1.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employess")
 public class Employee {
+    private Long employeeId;
 }
