3da90b6030345f05855ed87fe3a35168a3e2549f
Thimachal <thimachal@gmail.com>
2023-11-16 20:04:31 -0300

    controlller

--- exemple1/src/main/java/com/exemple1/exemple1/Services/PostService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/PostService.java
@@ -1,4 +1,13 @@
 package com.exemple1.exemple1.Services;
 
+import com.exemple1.exemple1.Repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PostService {
+    @Autowired
+    private PostRepository actionPostRepository;
+
+    public void postAll(){
+        actionPostRepository.findAll();
+    }
 }
