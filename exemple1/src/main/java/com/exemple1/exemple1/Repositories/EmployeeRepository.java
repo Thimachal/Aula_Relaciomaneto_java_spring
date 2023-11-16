7752f6cd906c7f8ac518273117e07ff0625cd4d2
Thimachal <thimachal@gmail.com>
2023-11-16 19:14:36 -0300

    repository

--- exemple1/src/main/java/com/exemple1/exemple1/Repositories/EmployeeRepository.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.exemple1.exemple1.Repositories;
 
 import com.exemple1.exemple1.Models.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 }
