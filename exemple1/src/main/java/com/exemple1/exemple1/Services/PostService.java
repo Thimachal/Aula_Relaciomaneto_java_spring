73d38383d3f29b5a2d9e04fea370bd3e8b53e3da
Thimachal <thimachal@gmail.com>
2023-11-16 20:11:19 -0300

    controlller

--- exemple1/src/main/java/com/exemple1/exemple1/Services/PostService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/PostService.java
@@ -2,12 +2,13 @@ package com.exemple1.exemple1.Services;
 
 import com.exemple1.exemple1.Repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 public class PostService {
     @Autowired
     private PostRepository actionPostRepository;
 
-    public void postAll(){
-        actionPostRepository.findAll();
+    public ResponseEntity<?> postAll(){
+        return ResponseEntity.ok(actionPostRepository.findAll());
     }
 }
