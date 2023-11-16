36240e0a0905f4787a28060e2779e554afc2b2e2
Thimachal <thimachal@gmail.com>
2023-11-16 18:52:07 -0300

    entidades

--- exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
@@ -1,11 +1,12 @@
 package com.exemple1.exemple1.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employess")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeId;
     private String employeeName;
 }
