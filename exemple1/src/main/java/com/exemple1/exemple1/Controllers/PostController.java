b7c1b1db60e1e9de7e7d042f126909a75fccdc48
Thimachal <thimachal@gmail.com>
2023-11-16 19:51:51 -0300

    repository

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
@@ -1,4 +1,12 @@
 package com.exemple1.exemple1.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostController {
+    @RequestMapping("/posts")
+    public class allPosts{
+
+    }
 }
