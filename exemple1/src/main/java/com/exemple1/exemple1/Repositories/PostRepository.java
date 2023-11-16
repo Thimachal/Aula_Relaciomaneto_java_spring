f89409dee1563c4b27fa5009ddf5d6254b5e4ec3
Thimachal <thimachal@gmail.com>
2023-11-16 19:54:34 -0300

    controlller

--- exemple1/src/main/java/com/exemple1/exemple1/Repositories/PostRepository.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Repositories/PostRepository.java
@@ -4,6 +4,10 @@ import com.exemple1.exemple1.Models.Post;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends CrudRepository<Post,Long> {
+
+    List<Post> findAll();
 }
