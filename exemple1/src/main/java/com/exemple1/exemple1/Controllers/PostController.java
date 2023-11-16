b0f698ed0ac5ed493dfca004c9e1cd9052c0f544
Thimachal <thimachal@gmail.com>
2023-11-16 19:59:38 -0300

    controlller

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/PostController.java
@@ -1,12 +1,16 @@
 package com.exemple1.exemple1.Controllers;
 
+import com.exemple1.exemple1.Services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
+    @Autowired
+    private PostService actionPostService;
     @RequestMapping("/posts")
-    public class allPosts{
-
+    public void classallPosts{
+        actionPostService.postAll();
     }
 }
