cdcf4e047ee8f92ff2d68c9e27722bd40b60c208
Thimachal <thimachal@gmail.com>
2023-11-17 22:37:45 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Models/Post.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/Post.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "posts")
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long postId;
     private String postName;
     private Double postSalary;
