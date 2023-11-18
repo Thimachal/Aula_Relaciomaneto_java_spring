dce44268ccadc80379e9699ac69ab936af13e360
Thimachal <thimachal@gmail.com>
2023-11-17 21:57:52 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
@@ -1,12 +1,16 @@
 package com.exemple1.exemple1.Services;
 
+import com.exemple1.exemple1.Repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRepository actionEmployeeRepository;
     public ResponseEntity<?> employeeAll(){
-        return
+        return ResponseEntity.ok(actionEmployeeRepository.findAll());
     }
 
 }
