636d1b6602759a3287e4292a81082369394e6614
Thimachal <thimachal@gmail.com>
2023-11-17 21:53:46 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.exemple1.exemple1.Controllers;
 
 import com.exemple1.exemple1.Services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,12 @@ public class EmployeeController {
     private EmployeeService actionEmployeeService;
 
     @GetMapping("/")
-    public ResponseEntity<?>
+    public ResponseEntity<?> allEmployees() {
+        try {
+            return new ResponseEntity<>(actionEmployeeService.emplyeeAll.getB)
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
 }
