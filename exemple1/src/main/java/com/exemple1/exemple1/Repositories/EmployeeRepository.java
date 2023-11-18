06f85e29ce06b81486c32e0461c4f8a6ea3280ba
Thimachal <thimachal@gmail.com>
2023-11-17 21:35:53 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Repositories/EmployeeRepository.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Repositories/EmployeeRepository.java
@@ -4,6 +4,9 @@ import com.exemple1.exemple1.Models.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+    List<Employee> findAll();
 }
