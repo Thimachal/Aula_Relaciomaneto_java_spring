c886bfbf097767044bd0426e80540bcd442040da
Thimachal <thimachal@gmail.com>
2023-11-17 21:34:26 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
@@ -1,4 +1,10 @@
 package com.exemple1.exemple1.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+
 }
