b32863bc9d19bd4c3e1fe55e1610d85895e63c07
Thimachal <thimachal@gmail.com>
2023-11-17 22:37:36 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/Employee.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "employess")
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long employeeId;
     private String employeeName;
 
