5392b24b08d62bf04bd324bcdb6cb11fa399c154
Thimachal <thimachal@gmail.com>
2023-11-17 21:59:18 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,8 @@ public class EmployeeController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @PostMapping("/")
+    public ResponseEntity<?>
 
 }
