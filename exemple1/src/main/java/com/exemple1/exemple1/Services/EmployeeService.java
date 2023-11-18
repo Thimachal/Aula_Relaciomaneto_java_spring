0a5930d93baa28b3635c17e1c78abddb6b5503c0
Thimachal <thimachal@gmail.com>
2023-11-17 21:55:27 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
@@ -1,8 +1,12 @@
 package com.exemple1.exemple1.Services;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-    public Object emplyeeAll;
+    public ResponseEntity<?> employeeAll(){
+        return
+    }
+
 }
