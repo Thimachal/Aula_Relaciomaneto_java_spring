e623857bb0fc3a8b83e91b8766593bdc04645f16
Thimachal <thimachal@gmail.com>
2023-11-17 21:50:50 -0300

    controller 2.0

--- exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Controllers/EmployeeController.java
@@ -1,10 +1,19 @@
 package com.exemple1.exemple1.Controllers;
 
+import com.exemple1.exemple1.Services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService actionEmployeeService;
+
+    @GetMapping("/")
+    public ResponseEntity<?>
 
 }
