a201739e2c2043130e3eaf28a82281f2cc7887be
Thimachal <thimachal@gmail.com>
2023-11-29 22:05:22 -0300

    dto

--- exemple1/src/main/java/com/exemple1/exemple1/Models/PostDTO.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/PostDTO.java
@@ -1,4 +1,14 @@
 package com.exemple1.exemple1.Models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class PostDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long postId;
+    private String postNameDTO;
+    private Double postSalaryDTO;
 }
