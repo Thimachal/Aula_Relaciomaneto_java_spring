4f0cd91d83d1852e0f67af5795c63166eecb706a
Thimachal <thimachal@gmail.com>
2023-11-29 22:58:27 -0300

    dto

--- exemple1/src/main/java/com/exemple1/exemple1/Models/EmployeeDTO.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Models/EmployeeDTO.java
@@ -11,4 +11,35 @@ public class EmployeeDTO {
 
     private Post postDTO;
 
+    public EmployeeDTO() {
+    }
+    public EmployeeDTO(Employee obj) {
+        this.employeeIDDTO = obj.getEmployeeId();
+        this.employeeNameDTO = obj.getEmployeeName();
+        this.postDTO = obj.getPost();
+    }
+
+    public Long getEmployeeIDDTO() {
+        return employeeIDDTO;
+    }
+
+    public void setEmployeeIDDTO(Long employeeIDDTO) {
+        this.employeeIDDTO = employeeIDDTO;
+    }
+
+    public String getEmployeeNameDTO() {
+        return employeeNameDTO;
+    }
+
+    public void setEmployeeNameDTO(String employeeNameDTO) {
+        this.employeeNameDTO = employeeNameDTO;
+    }
+
+    public Post getPostDTO() {
+        return postDTO;
+    }
+
+    public void setPostDTO(Post postDTO) {
+        this.postDTO = postDTO;
+    }
 }
