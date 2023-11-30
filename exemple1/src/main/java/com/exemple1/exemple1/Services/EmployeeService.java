b278078fd403b877ad01a0c9f425ddcf72b0e748
Thimachal <thimachal@gmail.com>
2023-11-29 22:58:44 -0300

    dto

--- exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.exemple1.exemple1.Services;
 
 import com.exemple1.exemple1.Models.Employee;
+import com.exemple1.exemple1.Models.EmployeeDTO;
+import com.exemple1.exemple1.Models.Post;
 import com.exemple1.exemple1.Repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +16,14 @@ public class EmployeeService {
         return ResponseEntity.ok(actionEmployeeRepository.findAll());
     }
 
-    public ResponseEntity<?> registerService(Employee employeeFromFront) {
-        Employee requestService = actionEmployeeRepository.save(employeeFromFront);
-        return ResponseEntity.ok(requestService);
+    public ResponseEntity<?> registerService(EmployeeDTO employeeFromFront) {
+        Employee employee = new Employee();
+
+        employee.setEmployeeId(employeeFromFront.getEmployeeIDDTO());
+        employee.setEmployeeName(employeeFromFront.getEmployeeNameDTO());
+        employee.setPost(employeeFromFront.getPostDTO());
+
+        actionEmployeeRepository.save(employee);
+        return ResponseEntity.ok(employee);
     }
 }
