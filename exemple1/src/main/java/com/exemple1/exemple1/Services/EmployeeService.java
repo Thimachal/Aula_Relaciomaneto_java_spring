0a811328b3f59db86f46384bccc3868b7a0e106a
Thimachal <thimachal@gmail.com>
2023-12-04 14:06:10 -0300

    dto

--- exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
+++ exemple1/src/main/java/com/exemple1/exemple1/Services/EmployeeService.java
@@ -4,28 +4,35 @@ import com.exemple1.exemple1.Models.Employee;
 import com.exemple1.exemple1.Models.EmployeeDTO;
 import com.exemple1.exemple1.Models.Post;
 import com.exemple1.exemple1.Repositories.EmployeeRepository;
+import com.exemple1.exemple1.Repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
     @Autowired
     private EmployeeRepository actionEmployeeRepository;
 
+    @Autowired PostRepository actionPostRepository;
 
     public ResponseEntity<?> registerService(EmployeeDTO employeeFromFront) {
-        Employee employee = new Employee();
-        Post post = new Post();
+        try {
+            Employee employee = new Employee();
+            Post post = actionPostRepository.findById(employeeFromFront.getPostIdDTO()).orElseThrow();
 
-        employee.setEmployeeId(employeeFromFront.getEmployeeIDDTO());
-        employee.setEmployeeName(employeeFromFront.getEmployeeNameDTO());
-        post.setPostId(employeeFromFront.getPostIdDTO());
+            employee.setEmployeeId(employeeFromFront.getEmployeeIDDTO());
+            employee.setEmployeeName(employeeFromFront.getEmployeeNameDTO());
+            employee.setPost(post);
 
-        actionEmployeeRepository.save(employee);
-
-
-        return ResponseEntity.ok(employee,post);
+            actionEmployeeRepository.save(employee);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     public ResponseEntity<?> employeeAll(){
