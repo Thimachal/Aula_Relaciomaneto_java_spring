f1c6f3a0c8957c1fb3890eb61c52de8074b03015
Thimachal <thimachal@gmail.com>
2023-12-04 21:41:02 -0300

    comments

--- exemple2/src/main/java/com/exemple2/exemple2/Repositories/CommentRepository.java
+++ exemple2/src/main/java/com/exemple2/exemple2/Repositories/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.exemple2.exemple2.Repositories;
 
-public interface CommentRepository {
+import com.exemple2.exemple2.Models.Comments;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comments, Long> {
 }
+
