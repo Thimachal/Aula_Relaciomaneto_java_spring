4f25b5121f9b6843b0b45e6f132f940024b2dbc6
Thimachal <thimachal@gmail.com>
2023-12-04 21:41:14 -0300

    comments

--- exemple2/src/main/java/com/exemple2/exemple2/Repositories/PostageRepository.java
+++ exemple2/src/main/java/com/exemple2/exemple2/Repositories/PostageRepository.java
@@ -1,4 +1,9 @@
 package com.exemple2.exemple2.Repositories;
 
-public interface PostageRepository {
+import com.exemple2.exemple2.Models.Postage;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostageRepository extends CrudRepository<Postage, Long> {
 }
