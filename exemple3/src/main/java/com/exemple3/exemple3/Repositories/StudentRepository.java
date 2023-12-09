bc540623a67b8a840e5848674b414121f63cd3d7
Thimachal <thimachal@gmail.com>
2023-12-08 23:05:41 -0300

    students

--- exemple3/src/main/java/com/exemple3/exemple3/Repositories/StudentRepository.java
+++ exemple3/src/main/java/com/exemple3/exemple3/Repositories/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.exemple3.exemple3.Repositories;
 
-public interface StudentRepository {
+import com.exemple3.exemple3.Models.Students;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Students, Long> {
 }
