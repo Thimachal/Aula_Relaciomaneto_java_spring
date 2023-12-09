a6339c0fcf90f57f8384288e412738663ef1f219
Thimachal <thimachal@gmail.com>
2023-12-08 22:42:17 -0300

    students

--- exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
+++ exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
@@ -2,6 +2,9 @@ package com.exemple3.exemple3.Models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "cursos")
 public class Courses {
@@ -9,4 +12,6 @@ public class Courses {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long courseId;
     private String courseName;
+    @ManyToMany
+    private List<Students> studentsList = new ArrayList<>();
 }
