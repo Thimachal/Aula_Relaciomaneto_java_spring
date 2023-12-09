f6c601e4585149780d300a78f628d1cc74ad7273
Thimachal <thimachal@gmail.com>
2023-12-08 22:53:17 -0300

    students

--- exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
+++ exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "cursos")
+@Table(name = "courses")
 public class Courses {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
