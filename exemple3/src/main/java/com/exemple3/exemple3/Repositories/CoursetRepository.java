6ea780c2290812fa812f5a4e765c6a52a38563f7
Thimachal <thimachal@gmail.com>
2023-12-08 23:05:18 -0300

    students

--- exemple3/src/main/java/com/exemple3/exemple3/Repositories/CoursetRepository.java
+++ exemple3/src/main/java/com/exemple3/exemple3/Repositories/CoursetRepository.java
@@ -1,4 +1,9 @@
 package com.exemple3.exemple3.Repositories;
 
-public interface CoursetRepository {
+import com.exemple3.exemple3.Models.Courses;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursetRepository extends CrudRepository<Courses, Long> {
 }
