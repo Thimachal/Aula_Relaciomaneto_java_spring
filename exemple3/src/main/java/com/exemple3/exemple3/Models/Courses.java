f418aa3042346d350b7e0e2a9240918cf1ee1530
Thimachal <thimachal@gmail.com>
2023-12-08 22:37:43 -0300

    students

--- exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
+++ exemple3/src/main/java/com/exemple3/exemple3/Models/Courses.java
@@ -1,4 +1,12 @@
 package com.exemple3.exemple3.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cursos")
 public class Courses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long courseId;
+    private String courseName;
 }
